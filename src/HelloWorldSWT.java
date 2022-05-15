83465551ff447dfee55a2cd719b5e86a9819beb2
magicman66 <maks15.stempihar@gmail.com>
2022-05-15 21:20:09 +0200

    Prvi testni push

---
+++ src/HelloWorldSWT.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Uporabnik
+ *
+ */
+public class HelloWorldSWT {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Ali deluje?");
+	}
+
+}
